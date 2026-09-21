b9918e0b95d064a37af90d7c130b474ff1c361ee
ganeshkmr82 <ganesh.kumarp@gmail.com>
2026-09-20 20:38:28 -0700

    updateSep20
    update on sept 20

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 # main.py
-# Run THIS file. Pybricks will automatically bundle robot.py, mission_*.py
-#  along with it since they're imported below.
+# Run THIS file. Pybricks will automatically bundle robot.py, mission_1.py,
+# mission_2.py, and mission_3.py along with it since they're imported below.
 #
 # LEFT / RIGHT  -> change selected mission number (wraps around)
 # CENTER        -> run the selected mission
 # After a mission finishes, it returns to the picker - just like SPIKE Prime.
-
 from robot import hub
 from pybricks.parameters import Button, Color
 from pybricks.tools import wait
 
-import mission_1 
+import mission_1
 import mission_2
 import mission_3
 import mission_4
 import mission_5
 import mission_6
+import mission_7
 hub.system.set_stop_button(Button.BLUETOOTH)
-missions = [mission_1, mission_2, mission_3, mission_4, mission_5, mission_6]
+missions = [mission_1, mission_2, mission_3, mission_4, mission_5, mission_6, mission_7]
 selected = 0
 prev_pressed = set()
 
---
+++ mission_7.py
@@ -0,0 +1,26 @@
+from robot import drive_base, left_arm, right_arm, set_speed
+from pybricks.parameters import Stop
+from pybricks.tools import wait
+
+def run():
+    wait(200)
+    drive_base.reset()
+    drive_base.use_gyro(True)
+    set_speed(speed=300, accel=600, turn_rate=100, turn_accel=250)
+    drive_base.straight(40)
+    drive_base.turn(-53)
+    drive_base.straight(495)
+    drive_base.turn(53)
+    set_speed(speed=225, accel=250)
+    drive_base.straight(140)
+    drive_base.turn(-90)
+    set_speed(speed=900, accel=800)
+    drive_base.straight(100)
+    drive_base.use_gyro(False)
+    drive_base.straight(-50)
+    '''
+    drive_base.turn(-37)
+    set_speed(turn_accel=300, speed=100)
+    drive_base.straight(30)
+    drive_base.use_gyro(False)
+    '''

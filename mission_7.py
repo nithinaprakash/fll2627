392dacc2c768c128be2255c6cbade9d961b1d0ea
ganeshkmr82 <ganesh.kumarp@gmail.com>
2026-09-21 19:26:54 -0700

    fixed drivelength

--- mission_7.py
+++ mission_7.py
@@ -15,9 +15,11 @@ def run():
     drive_base.straight(140)
     drive_base.turn(-90)
     set_speed(speed=900, accel=800)
-    drive_base.straight(100)
+    drive_base.straight(107)
     drive_base.use_gyro(False)
     drive_base.straight(-50)
+    drive_base.turn(10)
+    drive_base.straight(-700)
     '''
     drive_base.turn(-37)
     set_speed(turn_accel=300, speed=100)
